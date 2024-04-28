b788be30b1bf2f5ff1987682cad2ec030c4793ce
unaveengoud <unaveengoud.006@gmail.com>
2024-04-28 19:29:17 +0530

    message

---
+++ SampleScripts/evennumbers.py
@@ -0,0 +1,24 @@
+# Python program to print Even Numbers in a List
+
+# list of numbers
+# list1 = [10, 21, 4, 45, 66, 93]
+
+# # iterating each number in list
+# for num in list1:
+
+# 	# checking condition
+# 	if num % 2 == 0:
+# 		print(num, end=" ")
+
+
+# Python program to print Even Numbers in given range
+
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
+
+# iterating each number in list
+for num in range(start, end + 1):
+
+	# checking condition
+	if num % 2 == 0:
+		print(num, end=" ")
